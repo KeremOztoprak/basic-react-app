ca3f1351db4de08ca4bcafede28df908cef7be99
Kerem Öztoprak <88899880+KeremOztoprak@users.noreply.github.com>
2023-03-17 01:34:37 +0300

    feat : creating a create page🎉

--- src/Create.js
+++ src/Create.js
@@ -2,6 +2,22 @@ const Create = () => {
     return (
         <div className="create">
             <h2 style={{ color: '#ff793f' }}>Yeni Yazı Ekle</h2>
+            <form>
+                <label>Yazı Başlık:</label>
+                <input type="text" required />
+                <label>Yazı Aciklama:</label>
+                <textarea required>
+
+                </textarea>
+                <label>Yazar:</label>
+                <select>
+                    <option value="Bugs Bunny">Bugs Bunny</option>
+                    <option value="Sylvester">Sylvester</option>
+                    <option value="Snoopy">Snoopy</option>
+                    <option value="Scooby-Doo">Scooby-Doo</option>
+                </select>
+                <button>Ekle</button>
+            </form>
         </div>
     );
 
